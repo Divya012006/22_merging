6153b90a686a2056667d0f95e0bbe679c1f42f87
Sony <kmdivyashree2006@gmail.com>
2025-05-24 13:12:56 +0530

    Basic html syntax

--- sunday2.cpp
+++ sunday2.cpp
@@ -2,5 +2,15 @@
 int main()
 {
 	printf("Hellow world");
+	printf("GOOD MORNING!!");
+	printf("hey i am learning c with harry\n");
+        printf("today is friday\n");
+        printf("hey the topic i am going to teach you is FUNCTIONS AND RECURRSIONS\n");
+        printf("HEY CURRENTLY I AM LIVING IN PAHALGAM\n");
+        printf("Youtube - C Language for Beginners (With Notes)");
+        printf("hey i am learning c with harry\n");
+        printf("hey \n");
+        printf(" i know VS CODE EDITORS is a GREAT APPLCATION\n");
+	printf("Because it has adopted all features which are very needed for current generation"); 
         return 0;
 }
