9feaeb34caa7b129073752e4fa269c3dcca7074e
Sony <kmdivyashree2006@gmail.com>
2025-05-24 13:19:16 +0530

    if condition

---
+++ sunday2.cpp
@@ -0,0 +1,17 @@
+//this is a simple  cpp program on if conditions
+
+#include <stdio.h>
+int main()
+{
+    int i=0;
+    while(i<21)
+    {
+        if(i>9)
+        {
+            printf("the natural numbers are : %d\n", i);
+        }
+        i++;
+    }
+    return 0;
+}
+
