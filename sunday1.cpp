308edf9804a22e849d8ced36b9b4a4022f9ea2c4
Sony <kmdivyashree2006@gmail.com>
2025-05-24 12:28:26 +0530

    a cpp program to find the greatest number among four digits

---
+++ sunday1.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int a1=45;
+    int a2=584;
+    int a3=36;
+    int a4=890;
+    if(a1>a2 && a1>a3 && a1>a4)
+    {
+        printf("a1 is the greatest of all", a1);
+    }
+    else if(a2>a1 && a2>a3 && a2>a4)
+    {
+        printf("a2 is the greatest of all", a2);
+    }
+    else if(a3>a1 && a3>a2 && a3>a4)
+    {
+        printf("a3 is the greatest of all", a3);
+    }
+    else if(a4>a2 && a4>a3 && a4>a1)
+    {
+        printf("a4 is the greatest of all", a4);
+    }
+    return 0;
+}
